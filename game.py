699320b949b5b050a48364363ee68dfd165c9d4a
gang7994 <gang7994@naver.com>
2022-02-11 20:13:37 +0900

    Add start_screen

--- game.py
+++ game.py
@@ -1,15 +1,27 @@
 import pygame
 
+def display_start_screen():
+    pygame.draw.circle(screen, WHITE, start_button.center,60, 5)
+
 pygame.init()
 screen_width = 1200
 screen_height = 720
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Chimpanzee Game")
 
+
+start_button = pygame.Rect(0,0,120,120)
+start_button.center = (120,screen_height-120)
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
 running = True
 while(running):
-     for event in pygame.event.get():
-         if event.type == pygame.QUIT:
-             running = False
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(BLACK)
+    display_start_screen()
+    pygame.display.update()
 pygame.quit()
